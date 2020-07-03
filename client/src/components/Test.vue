f4676254b93cdd49dad35227777b3b2200033219
Matt Moore <mattmoore@pop-os.localdomain>
2020-07-03 14:57:00 -0400

    wired up vueapollo

--- client/src/components/Test.vue
+++ client/src/components/Test.vue
@@ -1,15 +1,34 @@
 <template>
   <div>
     <h1>Querying GraphQL via Apollo in VueJS</h1>
-    <h2>{{ msg }}</h2>
+    <h2>{{ msg }} {{ artists }} queries are: {{this.$apollo.queries}}</h2>
+    <ApolloQuery :query="this.apollo.artists.query">
+      <template v-slot="{ result: { loading, error, data } }">
+        <div v-if="loading" class="loading apollo">Loading...</div>
+        <div v-else-if="error" class="error apollo">An error occurred</div>
+        <div v-else-if="data" class="result apollo">{{ data.artists }}</div>
+        <div v-else class="no-result apollo">No result :(</div>
+      </template>
+    </ApolloQuery>
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Prop, Vue } from "vue-property-decorator";
+import gql from "graphql-tag";
+import register from "../hooks";
 
 @Component
 export default class Test extends Vue {
   @Prop() private msg!: string;
+  get apollo () {
+    return {
+      artists: {
+        query: gql`{artists {name}}`,
+        prefetch: true
+      }
+    }
+  }
+  artists: string[] = ["foo"];
 }
 </script>
--- client/src/main.ts
+++ client/src/main.ts
@@ -1,8 +1,22 @@
 import Vue from "vue";
 import App from "./App.vue";
+import ApolloClient from "apollo-boost";
+import VueApollo from "vue-apollo";
 
 Vue.config.productionTip = false;
 
+const apolloClient = new ApolloClient({
+  // You should use an absolute URL here
+  uri: "http://localhost:4000"
+});
+
+const apolloProvider = new VueApollo({
+  defaultClient: apolloClient
+});
+
+Vue.use(VueApollo);
+
 new Vue({
+  apolloProvider,
   render: h => h(App)
 }).$mount("#app");
